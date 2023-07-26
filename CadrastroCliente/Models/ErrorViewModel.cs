050088d86b469fc49efb563eaaf0de22f2661a3d
André Silva <silvestrinisilvaandre@gmail.com>
2023-07-26 14:55:40 -0300

    Primeira versão do projeto

---
+++ CadrastroCliente/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CadrastroCliente.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
